fee68033648d7dc2adc36fc6a3bff51b18945ed7
MackendyTechnology <bouquetmackendy34@gmail.com>
2023-09-30 14:38:00 -0400

    react-project

---
+++ react-project/src/App.jsx
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <div className="app">
+      <img src="/img react.png" alt="logo" />
+      <h1>I learn react</h1>
+      <p>React is amazing</p>
+      <div>
+        <button>My button</button>
+        <button>I love react</button>
+      </div>
+      <hr />
+      <h2>React</h2>
+    <p>
+    React est une bibliothèque JavaScript populaire utilisée pour construire des interfaces utilisateur interactives et réactives. 
+    Elle a été développée par Facebook et est largement utilisée dans le développement web. 
+    React utilise une approche basée sur les composants, 
+    où les différentes parties de l`interface utilisateur sont divisées en composants réutilisables. 
+    Ces composants peuvent être mis à jour de manière efficace et réactive, 
+    ce qui permet de créer des applications web dynamiques et performantes. 
+    React est souvent utilisé en combinaison avec d`autres bibliothèques ou frameworks, 
+    tels que Redux ou Next.js, pour créer des applications web plus complexes.
+    </p>
+    </div>
+  );
+}
